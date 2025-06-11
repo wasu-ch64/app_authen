69e874c8bd259b03cdd9ca320feaf341065e0c76
Mganga <wasuchailangka0@gmail.com>
2025-06-11 20:25:10 +0700

    update tsconfig

--- server/jest.config.ts
+++ server/jest.config.ts
@@ -3,6 +3,7 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  rootDir: '.',
   testMatch: ['**/tests/**/*.test.ts'],
   moduleFileExtensions: ['ts', 'js', 'json'],
   clearMocks: true,
