9d17debf0ae71510467b72f451e40fdf7c4228e2
Mganga <wasuchailangka0@gmail.com>
2025-08-15 20:33:02 +0700

    up

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 // const API_URL = import.meta.env.VITE_API_URL + "/api";
-const API_URL = "__VITE_API_URL__" + "/api";
+const API_URL = "/api";
 
 export const axiosInstance = axios.create({
   baseURL: API_URL,
